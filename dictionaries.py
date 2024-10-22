d43ffd984d0539bf730736635a6c4bc62ceda179
Alekh Sachan <137406174+alekh08@users.noreply.github.com>
2024-10-22 16:14:32 +0530

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,47 @@
+
+'''
+a={"name":"Alekh","age":18}
+print(a)
+print(type(a))
+
+# output #
+
+{'name': 'alekh', 'age': 18}
+<class 'dict'>
+
+''
+
+
+a={"name":"Alekh","age":18}
+print(a["name"])
+
+#  output  #
+Alekh
+
+''
+a={"name":"Alekh","age":18}
+print(a.get("age"))
+
+#output:
+18
+
+'''
+#membership operator/check
+a={"name":"Alekh","age":18,"city":"Kanpur"}
+print('name' in a)
+
+#output: True
+
+
+a={"name":"Alekh","age":18,"city":"Kanpur"}
+a['caste']='OBC'
+print(a)
+
+#output: {'name': 'Alekh', 'age': 18, 'city': 'Kanpur', 'caste': 'OBC'}
+
+
+
+
+
+
+
